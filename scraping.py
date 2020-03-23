c0eb8de476831948095a5a4fd98698e924a6c0a4
Skylar Fung <skylarfung@outlook.com>
2020-03-23 16:33:35 -0700

    webpage uploads mars hemi img

--- scraping.py
+++ scraping.py
@@ -18,7 +18,11 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "cerberus": cerberus(browser),
+        "schiaparelli": schiaparelli(browser),
+        "sytris": syrtis(browser),
+        "valles": valles(browser)
     }
 
     return data
